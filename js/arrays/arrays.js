6889dc3fac91f97a2317df24e7230035f79b5325
timothyAgevi <timothyagevi@gmail.com>
2022-01-22 18:56:28 +0300

    add insert algo

--- js/arrays/arrays.js
+++ js/arrays/arrays.js
@@ -1,13 +1,24 @@
+//Insert
 class myArray{
-    myArray(size,length){
-     let size=size
-     let length=length
-     
+   constructor(size,length,arr){
+     this.size=size
+     this.length=length
+     this.arr= arr
 }
-insert(i,val){
-    if (i==!2){
-        i=i+1
+myInsert(index,val){
+    if(this.length<this.size){
+        for( let i=this.length-1;i>=index;i--){
+          this.arr[i+1]=this.arr[i];
+          
+        }
+     this.length ++;
+       this.arr[index] = val 
     }
-
 }
 }
+// [1,2,3,4,5,6,,,]
+//insert(2,7)
+var arr= new myArray( 10,6 ,[1,2,3,4,5,6,]) 
+console.log(arr) 
+ arr.myInsert(2,7)
+console.log(arr)  
