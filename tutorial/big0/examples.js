3615d67446b3fba78b33901a37e666c595841d8d
timothyAgevi <timothyagevi@gmail.com>
2022-03-20 23:59:07 +0300

    add example5

--- tutorial/big0/examples.js
+++ tutorial/big0/examples.js
@@ -35,3 +35,15 @@ function onlyElementsAtEvenIndex(array) {
     }
     return newArray;
 }
+//qn5 :o(n)
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
