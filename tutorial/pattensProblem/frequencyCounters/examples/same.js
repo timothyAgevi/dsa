3249abc7dd8f8377dbdb7bcde3e827f34ab0c163
timothyAgevi <timothyagevi@gmail.com>
2022-03-22 21:17:59 +0300

    add instruction for same function

---
+++ tutorial/pattensProblem/frequencyCounters/examples/same.js
@@ -0,0 +1,9 @@
+//write a function caklled same that takes 2 arrays.
+//returns true if every value in the array has it's corresponding value
+// squared in the second array.The frequencies (number of counts)must be the same length
+
+//note order doesnt matter for
+ examples
+ // same([1,2,3] , [4,1,9])// true 
+ //same([1,2,3], [1,9])//false
+ //same([1,2,1] ,[4,4,1])//false must be same frequency
