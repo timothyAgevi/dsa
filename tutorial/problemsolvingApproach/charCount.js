387ebdcf629d5480cee66314eaa0c07701be2c30
timothyAgevi <timothyagevi@gmail.com>
2022-03-22 16:24:17 +0300

    add refactored solution

--- tutorial/problemsolvingApproach/charCount.js
+++ tutorial/problemsolvingApproach/charCount.js
@@ -36,3 +36,30 @@ return result;
 }
  
  console.log(charCount("hello !"))
+
+ //refactor solution using char of instead of for i <str.length >
+ function charCount1(str){
+    //make object to return at end
+    var result ={};
+    //loop over str,foreach char
+    for(var i =0;i<str.length;i++){
+       // variable for str[i] 
+       var char =str[i].toLowerCase()
+       //add regular experession to result //TO CHECKALPHANUMERIC
+       if(/[a-z0-9]/.test(char)){
+       if(result[char]>0){//char exists in str
+        ////if char is a number/letter and key in object ,add one to count object
+          result[char]++;
+       }else{
+     //if char is a number/letter a not  in object ,add it and set value to 1 .
+           result[char] =1;//char not yet in result object
+       };
+       
+    }
+    }
+     //if char issth else(soace,period etc)dont do anything
+    //return object at end
+    return result;
+    }
+     
+     console.log(charCount("hello !"))
