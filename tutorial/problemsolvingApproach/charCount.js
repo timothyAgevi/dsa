4da63f5bfa758f1980751e9c1db9ec63b0a3437e
timothyAgevi <timothyagevi@gmail.com>
2022-03-22 15:09:47 +0300

    break down problem

---
+++ tutorial/problemsolvingApproach/charCount.js
@@ -0,0 +1,21 @@
+// function charCount(str){
+//     //do something
+//     //return objects that are lowercase alphanumeric,case insensitive 
+//     //i.e convert to lowercase and
+// }
+
+
+    // examples
+// charcount('hello world')
+// charcount('Your PIN  is 1234 !')
+// charcount("")
+// charcount("*")
+
+function charCount(str){
+//make object to return at end
+//loop over str,foreach char
+   //if char is a number/letter and key in object ,add one to count object
+     //if char is a number/letter a not  in object ,add it and set value to 1 .
+      //if char issth else(soace,period etc)dont do anything
+//return object at end
+}
