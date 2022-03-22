87cb6ba7607de88140d7bb8a33c2da5470f2d21c
timothyAgevi <timothyagevi@gmail.com>
2022-03-22 15:42:11 +0300

    return result

--- tutorial/problemsolvingApproach/charCount.js
+++ tutorial/problemsolvingApproach/charCount.js
@@ -19,14 +19,17 @@ for(var i =0;i<str.length;i++){
    // variable for str[i] 
    var char =str[i]
    if(result[char]>0){//char exists in str
-      result[char]
+    ////if char is a number/letter and key in object ,add one to count object
+      result[char]++;
    }else{
-       result[char] =1;//char not yet inresult object
-   }
+ //if char is a number/letter a not  in object ,add it and set value to 1 .
+       result[char] =1;//char not yet in result object
+   };
+   
+}
+ //if char issth else(soace,period etc)dont do anything
+//return object at end
+return result;
 }
 
-   //if char is a number/letter and key in object ,add one to count object
-     //if char is a number/letter a not  in object ,add it and set value to 1 .
-      //if char issth else(soace,period etc)dont do anything
-//return object at end
-}
+charCount("hello")
