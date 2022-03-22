2d3673dd805ba44d16a86c9e56119320ef682b52
timothyAgevi <timothyagevi@gmail.com>
2022-03-22 15:27:13 +0300

    add if char exists in str

--- tutorial/problemsolvingApproach/charCount.js
+++ tutorial/problemsolvingApproach/charCount.js
@@ -16,8 +16,13 @@ function charCount(str){
 var result ={};
 //loop over str,foreach char
 for(var i =0;i<str.length;i++){
-    
+   // variable for str[i] 
+   var char =str[i]
+   if(result[char]>0){//char exists in str
+
+   }
 }
+
    //if char is a number/letter and key in object ,add one to count object
      //if char is a number/letter a not  in object ,add it and set value to 1 .
       //if char issth else(soace,period etc)dont do anything
