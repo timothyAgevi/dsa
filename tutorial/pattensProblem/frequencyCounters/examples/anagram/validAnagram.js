650bea35180373d165d79b84baeae19f08a7fd4c
timothyAgevi <timothyagevi@gmail.com>
2022-03-23 19:34:29 +0300

    create counter for str1

--- tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
+++ tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
@@ -10,8 +10,15 @@ if( str1.length !== str2.length){
 //create counter for both Strings
 let counter1={}
 let counter2={}
+//create arrs for  each string
+let arrStr1=str1.split('')
+let arrStr2= str2.split('')
 //loop  each val of  each arr
-   //create counters here
+for( val of  arrStr1){
+//create counters here
+   counter1[val]=( counter1[val] ||0) + 1
+}
+   
 // loop each key of frequencyCounter1
    // check if key in str1 is in str2
    //check if frequency of element in str1 ==str2 
