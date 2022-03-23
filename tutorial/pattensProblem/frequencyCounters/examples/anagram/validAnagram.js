11fae658b024072444782c9bdce757309f27e137
timothyAgevi <timothyagevi@gmail.com>
2022-03-23 19:21:03 +0300

    add complete pseudo code

---
+++ tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
@@ -0,0 +1,14 @@
+//Given 2 strings write a function to determine if the second string is an anagram of the first.
+
+//pseidocode
+//function taking 2 parameters
+function validAnagram(str1,st2){
+//check if str1.length==str2.lenghth
+//create counter for both Strings
+//loop  each val of  each arr
+   //create counters here
+// loop each key of frequencyCounter1
+   // check if key in str1 is in str2
+   //check if frequency of element in str1 ==str2 
+   
+}
