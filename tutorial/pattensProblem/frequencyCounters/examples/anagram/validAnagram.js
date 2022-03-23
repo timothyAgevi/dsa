fe3323cb8b32051109e0f310c28dcc4b370bb3a9
timothyAgevi <timothyagevi@gmail.com>
2022-03-23 19:26:54 +0300

    create counter for both Strings

--- tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
+++ tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
@@ -2,13 +2,18 @@
 
 //pseidocode
 //function taking 2 parameters
-function validAnagram(str1,st2){
+function validAnagram(str1,str2){
 //check if str1.length==str2.lenghth
+if( str1.length !== str2.length){
+    return false;          
+}
 //create counter for both Strings
+let counter1={}
+let counter2={}
 //loop  each val of  each arr
    //create counters here
 // loop each key of frequencyCounter1
    // check if key in str1 is in str2
    //check if frequency of element in str1 ==str2 
-   
+
 }
