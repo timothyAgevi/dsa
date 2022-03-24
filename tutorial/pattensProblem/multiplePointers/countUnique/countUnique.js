be27c76b8a1228271c0fc6468e59bd36f8d9804c
timothyAgevi <timothyagevi@gmail.com>
2022-03-25 00:36:24 +0300

    add countUniqueValues notes

--- tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
+++ tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
@@ -1,3 +1,5 @@
 // write function countUniqueVallues that accepts sorted array,
 //and counts the unique values in the array.
 //there can be negative numbers in array,but it will be always sorted.
+
+//cases
