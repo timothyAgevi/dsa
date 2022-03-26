597958ee82650e00789dd07ddafe8b62bfccff02
timothyAgevi <timothyagevi@gmail.com>
2022-03-26 19:02:17 +0300

    summary for frequencyCounter for strings

--- tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
+++ tutorial/pattensProblem/frequencyCounters/examples/anagram/validAnagram.js
@@ -16,7 +16,7 @@
         }
       
         const lookup = {};
-      
+      //creating counter called lookup
         for (let i = 0; i < first.length; i++) {
           let letter = first[i];
           // if letter exists, increment, otherwise set to 1
@@ -39,3 +39,13 @@
       
       // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
       validAnagram1('anagrams', 'nagaramm')
+
+      //summary 
+      //1,check if length are same.length
+      //2. create counter :loop each index of fisrt string
+                           //-create letter for each index in the first string.
+                          //- if letter exists, increment, otherwise set to 1 
+      //3.check if letter is in counter: -loop thru indexes of string 
+                                         //- create var to store each character.
+                                         //-   if (!lookup[letter]) :false else lookup[letter] - =1
+      // return true                                    
