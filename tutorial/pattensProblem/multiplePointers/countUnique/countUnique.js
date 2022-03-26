b410c48f14397e8ee9ae29915ff058f278fde70c
timothyAgevi <timothyagevi@gmail.com>
2022-03-26 18:18:20 +0300

    console before returning

--- tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
+++ tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
@@ -3,14 +3,18 @@
 //there can be negative numbers in array,but it will be always sorted.
 function countUniqueValues(arr){
     if(arr.length === 0) return 0;
-    var i = 0;
+    var i = 0;// index at zero
     for(var j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){
             i++;
             arr[i] = arr[j]
         }
     }
+    console.log(i +1)
     return i + 1;
+    
 }
-countUniqueValues([1,2,2,5,7,7,99]) //1,2
+countUniqueValues([1,2,2,5,7,7,9,9]) //1,2
+//countUniqueValues([1,2,3,4,4,5]) 1,2,3,5
+
 // try without changing array.
