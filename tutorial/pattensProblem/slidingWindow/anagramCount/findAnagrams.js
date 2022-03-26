e4c7fe46fb5ac460dce8e54f8912bf70bf1f2a92
timothyAgevi <timothyagevi@gmail.com>
2022-03-27 00:35:34 +0300

    check if values are equivalent

--- tutorial/pattensProblem/slidingWindow/anagramCount/findAnagrams.js
+++ tutorial/pattensProblem/slidingWindow/anagramCount/findAnagrams.js
@@ -19,6 +19,39 @@
 
  var findAnagram =function (s,p){
      const sArr=new Array(26).fill(0);
-     console.log(sArr);
+    //  console.log(sArr);
+     const pArr=new Array(26).fill(0);
+    //  console.log(pArr);
+     const result =[];
+
+     for(let i=0;i<p.length;i++){
+         let index=p.charCodeAt(i) % 26;
+         pArr[index]++;
+        //  console.log(index);
+        //  console.log(pArr[index]);
+        //  console.log(pArr)
+     }
+     for(let i=0;i<s.length;i++){
+        let index=s.charCodeAt(i) % 26;        
+        sArr[index]++;
+         console.log(index);
+         console.log(sArr[index]);
+         console.log(sArr)
+         //delete window's prevoius index 
+         if(i > p.length -1){
+             let headInx=s.charCodeAt(i - p.length) % 26  // i -p.length so as to remove previous index 
+             sArr[headInx]--;
+         }
+         //add  window's  new front index
+         if(i >=p.length -1){
+            //check if values are equivalent
+            if(arrayValuesEqual(sArr,pArr)){
+                result.push(i -(p.length -1)) // push from beginning
+            
+            }
+         }
+        
 
  }
+}
+ findAnagram("cbaebabacd","aabc")
