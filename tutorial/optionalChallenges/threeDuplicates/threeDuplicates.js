23270f6b777669d3f3d90645d08339e58675c187
timothyAgevi <timothyagevi@gmail.com>
2022-03-28 00:29:07 +0300

    hint

--- tutorial/optionalChallenges/threeDuplicates/threeDuplicates.js
+++ tutorial/optionalChallenges/threeDuplicates/threeDuplicates.js
@@ -1,1 +1,2 @@
 //write a function called, areThreeDuplicates whuch accepts a variable number of aurguments, and checks whether there are any duplicates among the arguments passed in.
+// can use frequency counter oer multiole duplicates pattern.
