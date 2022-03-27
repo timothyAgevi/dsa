2a825910e855b036285a1554a2d612cd005e08f4
timothyAgevi <timothyagevi@gmail.com>
2022-03-28 00:55:04 +0300

    pseudocode for threeDuplicates

--- tutorial/optionalChallenges/threeDuplicates/threeDuplicates.js
+++ tutorial/optionalChallenges/threeDuplicates/threeDuplicates.js
@@ -1,2 +1,14 @@
 //write a function called, areThreeDuplicates whuch accepts a variable number of aurguments, and checks whether there are any duplicates among the arguments passed in.
 // can use frequency counter oer multiole duplicates pattern.
+// pseudocode
+//1.convert args to array.
+//2.edge case: array empty,return false
+// else using multiple pointers
+//3.let var i=0
+//4.loop over array wirh j starting from 1. and
+   //4.check if arr[i] !===arr[j], increment i, continue
+//5. else return true;
+var areThreeDuplicates= function(args){
+
+}
+
