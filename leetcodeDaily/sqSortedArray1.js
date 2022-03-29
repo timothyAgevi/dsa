ea6f27da5699413cdf2337d51aa42c00dccef0d1
timothyAgevi <timothyagevi@gmail.com>
2022-03-29 12:27:46 +0300

    add  second solution

--- leetcodeDaily/sqSortedArray1.js
+++ leetcodeDaily/sqSortedArray1.js
@@ -1,18 +1,3 @@
-// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
-
- 
-
-// Example 1:
-
-// Input: nums = [-4,-1,0,3,10]
-// Output: [0,1,9,16,100]
-// Explanation: After squaring, the array becomes [16,1,0,9,100].
-// After sorting, it becomes [0,1,9,16,100].
-// Example 2:
-
-// Input: nums = [-7,-3,2,3,11]
-// Output: [4,9,9,49,121]
-
 //we crete 2 functions i.e numArrays,sumRange to enable us call sumRange multiple times.
 var NumArray = function(nums) {
     this.NumArray = [...nums]//spread nums ,use this operator to make it acessible
@@ -25,7 +10,5 @@ NumArray.prototype.sumRange = function(left, right) {
         sum += this.NumArray[left] 
         left++
     }
-    return sum;
+    return sum
 };
-
-
