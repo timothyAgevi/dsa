a4c3cbadfa84a0d7f0a62b3dd21ba416cc36f3fe
timothyAgevi <timothyagevi@gmail.com>
2022-03-29 12:29:10 +0300

    second solution as a ES6 class

--- leetcodeDaily/sqSortedArray1.js
+++ leetcodeDaily/sqSortedArray1.js
@@ -1,14 +1,16 @@
 //we crete 2 functions i.e numArrays,sumRange to enable us call sumRange multiple times.
-var NumArray = function(nums) {
-    this.NumArray = [...nums]//spread nums ,use this operator to make it acessible
-};
-//function to get sum of range
-//prototype.sumRange adds property sumRange to NumArray
-NumArray.prototype.sumRange = function(left, right) {
-    var sum = 0//initialize sum to zero
-    while (left <= right) { //iterate from keft to riht
-        sum += this.NumArray[left] 
-        left++
+class NumArray {
+    constructor(nums) {
+        this.NumArray = [...nums]; //spread nums ,use this operator to make it acessible
     }
-    return sum
-};
+    //function to get sum of range
+    //prototype.sumRange adds property sumRange to NumArray
+    sumRange(left, right) {
+        var sum = 0; //initialize sum to zero
+        while (left <= right) { //iterate from keft to riht
+            sum += this.NumArray[left];
+            left++;
+        }
+        return sum;
+    }
+}
