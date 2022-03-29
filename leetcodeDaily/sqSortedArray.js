34055e9a7c6aa2d00dbb13eb36dd8687b94d43c4
timothyAgevi <timothyagevi@gmail.com>
2022-03-29 12:26:11 +0300

    solution

--- leetcodeDaily/sqSortedArray.js
+++ leetcodeDaily/sqSortedArray.js
@@ -13,3 +13,17 @@
 // Input: nums = [-7,-3,2,3,11]
 // Output: [4,9,9,49,121]
 
+//we crete 2 functions i.e numArrays,sumRange to enable us call sumRange multiple times.
+var NumArray = function(nums) {
+    this.NumArray = [...nums]//spread nums ,use this operator to make it acessible
+};
+//function to get sum of range
+//prototype.sumRange adds property sumRange to NumArray
+NumArray.prototype.sumRange = function(left, right) {
+    var sum = 0//initialize sum to zero
+    while (left <= right) { //iterate from keft to riht
+        sum += this.NumArray[left] 
+        left++
+    }
+    return sum
+};
