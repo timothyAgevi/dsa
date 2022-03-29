376dd6051d9fc7a730ad0b20f07bc66075e49573
timothyAgevi <timothyagevi@gmail.com>
2022-03-29 12:51:16 +0300

    update instruction

--- leetcodeDaily/sqSortedArray.js
+++ leetcodeDaily/sqSortedArray.js
@@ -1,18 +1,27 @@
-// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+// Given an integer array nums, handle multiple queries of the following type:
 
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
  
 
 // Example 1:
 
-// Input: nums = [-4,-1,0,3,10]
-// Output: [0,1,9,16,100]
-// Explanation: After squaring, the array becomes [16,1,0,9,100].
-// After sorting, it becomes [0,1,9,16,100].
-// Example 2:
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
 
-// Input: nums = [-7,-3,2,3,11]
-// Output: [4,9,9,49,121]
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
 
+//solution
 //we crete 2 functions i.e numArrays,sumRange to enable us call sumRange multiple times.
 var NumArray = function(nums) {
     this.NumArray = [...nums]//spread nums ,use this operator to make it acessible
