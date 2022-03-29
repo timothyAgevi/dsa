0a4e15555068c09409bac555b9ec7086587b47ca
timothyAgevi <timothyagevi@gmail.com>
2022-03-29 23:40:53 +0300

    update countUnique values

--- tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
+++ tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
@@ -7,17 +7,15 @@ function countUniqueValues(arr){
     for(var j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){
             i++;
-            arr[i] = arr[j]
-            console.log(i,'\n',arr) 
-            
-           
-        }
+            arr[i] = arr[j];                   
+                }
     }
-    // console.log(i +1)
+
+    // console.log(i + 1)
     return i + 1;
     
 }
-countUniqueValues([1,2,3,2])
+countUniqueValues([1,2,3])
 // countUniqueValues([1,2,2,5,7,7,9,9]) //1,2
 //countUniqueValues([1,2,3,4,4,5]) 1,2,3,5
 
