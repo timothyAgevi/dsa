fe5c54bc6540dcc37a77269e0958b4fea4df6ca6
timothyAgevi <timothyagevi@gmail.com>
2022-03-30 23:21:31 +0300

    instructions to maxSum

---
+++ tutorial/optionalChallenges/slidingWindow/maxsum/maxSubArray.js
@@ -0,0 +1,3 @@
+// Given an array of integers and a number,
+//write a function caleed maxSubarraySum,which finds the maximum sum of subarray
+// with the length of the number passed to the function. 
