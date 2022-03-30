6b39a345e52582c3d7cda1d9df20d3dfd0766414
timothyAgevi <timothyagevi@gmail.com>
2022-03-30 23:35:49 +0300

    add edge case before operations

--- tutorial/optionalChallenges/slidingWindow/maxsum/maxSubArray1.js
+++ tutorial/optionalChallenges/slidingWindow/maxsum/maxSubArray1.js
@@ -1,9 +1,10 @@
 //rite a functio maxSubarraySum that accepts an array of integers and a number called n.
  //the function should calculate the  maxium sum of n consecutive elements in array.
  function maxSubarraySum(arr, num){
+    if (arr.length < num) return null;//edge case: if num greater than arr.length
     let maxSum = 0;
     let tempSum = 0;
-    if (arr.length < num) return null;//edge case: if num greater than arr.length
+   
     //loop thru num
     for (let i = 0; i < num; i++) {
       maxSum += arr[i];// set maxSum
