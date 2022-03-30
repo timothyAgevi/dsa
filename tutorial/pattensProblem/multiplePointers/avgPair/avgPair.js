bc4cce35ef04235f50ba674441c990a2841e2665
timothyAgevi <timothyagevi@gmail.com>
2022-03-30 17:06:28 +0300

    sln2.js incorrect

--- tutorial/pattensProblem/multiplePointers/avgPair/avgPair.js
+++ tutorial/pattensProblem/multiplePointers/avgPair/avgPair.js
@@ -2,3 +2,7 @@
 //determine if there is a pair of values in the aray where the avg of pairs equals the target average.
 //there may be more than one pair that matches average target.
 //constrints: time o(n),space o(1)
+
+//pseudocode
+//1.variables -left,right,average
+//2.loop over
---
+++ tutorial/pattensProblem/multiplePointers/avgPair/sln2.js
@@ -0,0 +1,13 @@
+function averagePair(rr){
+    // add whatever parameters you deem necessary - good luck!
+    let left = 0 ; 
+    let right = arr.length - 1; 
+    
+    let mid ;
+    
+    while(left < right){
+        mid = left + Math.floor((right - left)/2);
+        if(nums[mid + 1]) return ;
+    }
+    
+    }
