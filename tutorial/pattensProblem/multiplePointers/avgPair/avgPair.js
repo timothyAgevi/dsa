8d67462ac9a6a37c432bc654c02a2dcb77ece082
timothyAgevi <timothyagevi@gmail.com>
2022-03-30 16:58:43 +0300

    add avgPair

---
+++ tutorial/pattensProblem/multiplePointers/avgPair/avgPair.js
@@ -0,0 +1,4 @@
+// write a function called averagePair. Given a sorted array of integers,and target average,
+//determine if there is a pair of values in the aray where the avg of pairs equals the target average.
+//there may be more than one pair that matches average target.
+//constrints: time o(n),space o(1)
