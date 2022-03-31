b9e0aafd811442136eff8bfd59d52e53eb723a05
timothyAgevi <timothyagevi@gmail.com>
2022-03-31 23:35:56 +0300

    examples of longest substring

--- tutorial/optionalChallenges/slidingWindow/longestSubstring/findLongestSubstring.js
+++ tutorial/optionalChallenges/slidingWindow/longestSubstring/findLongestSubstring.js
@@ -1,2 +1,5 @@
 // write a function called findLongestSubString which accepts a string,
 // and returns the length of the longest substring with all distinct characters.
+
+//examples:
+// findLongestSubString('')//0
