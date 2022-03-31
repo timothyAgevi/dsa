1ad0d7655b88c955cb41d674d5265ac60274cd6d
timothyAgevi <timothyagevi@gmail.com>
2022-03-31 12:13:30 +0300

    complete minSubArray

--- tutorial/optionalChallenges/slidingWindow/minSubArrayLen1.js
+++ tutorial/optionalChallenges/slidingWindow/minSubArrayLen1.js
@@ -5,11 +5,11 @@
 //of which the sum is greater than  or equal to the integer passed.
 
 //examples
-//minSubArrayLen([2,3,1,2,4,3,],7)// 2  because [4,3 ]is the smallestsubarray
+//minSubArrayLen([2,3,1,2,4,3],7)// 2  because [4,3 ]is the smallestsubarray
 
 function minSubArrayLen(nums, sum) {
     let total = 0;
-    let start = 0;
+    let start = 0 ;
     let end = 0 ;
     let minLen = Infinity;
    
@@ -27,7 +27,7 @@ function minSubArrayLen(nums, sum) {
               total -= nums[start];
               start++;
       } 
-      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+      // current total less than required total(sum) but we reach the end, need this or else we'll be in an infinite loop 
       else {
         break;
       }
