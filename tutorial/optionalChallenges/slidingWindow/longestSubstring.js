21e02bd51580309e49de0c2d9cc0ac792c033838
timothyAgevi <timothyagevi@gmail.com>
2022-03-31 23:39:57 +0300

    delete prevoius longestSubstring

--- tutorial/optionalChallenges/slidingWindow/longestSubstring.js
+++
@@ -1,9 +0,0 @@
-// write a function called findLongestString ,which accepts  astring and returns the length
-// of the longest substring with all  distrinct characters.
-
-//examples:
-//findLongestString('')//o
-//findLongestString('rithmschool')//7
-//findLongestString('thisisawesome')//6
-//findLongestString('thecatinthe hat')
-
