ee66989a83f18011dd0ff2cc4af35cd94309456b
timothyAgevi <timothyagevi@gmail.com>
2022-03-31 13:58:22 +0300

    examples of longestSubstring

--- tutorial/optionalChallenges/slidingWindow/longestSubstring.js
+++ tutorial/optionalChallenges/slidingWindow/longestSubstring.js
@@ -1,3 +1,9 @@
 // write a function called findLongestString ,which accepts  astring and returns the length
 // of the longest substring with all  distrinct characters.
 
+//examples:
+//findLongestString('')//o
+//findLongestString('rithmschool')//7
+//findLongestString('thisisawesome')//6
+//findLongestString('thecatinthe hat')
+
