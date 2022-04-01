cc95cc054c42aa8ad854954b1f5ffd917ffddc10
timothyAgevi <timothyagevi@gmail.com>
2022-04-01 12:53:36 +0300

    add aproach 2 to longestSubstring

---
+++ tutorial/optionalChallenges/slidingWindow/longestSubstring/lengthOfLongestSubstring.js
@@ -0,0 +1,8 @@
+//leetcode 3
+// write a function called findLongestSubString which accepts a string,
+// and returns the length of the longest substring with all distinct characters.
+
+//examples:
+// findLongestSubString('')//0
+//findlongesrSubstring('rithmschool)//6
+//findLongestSubString('thecatinthehat')//7
