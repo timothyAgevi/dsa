e48e79bc4cf9b317a16efa7d96ba7acf529c786e
timothyAgevi <timothyagevi@gmail.com>
2022-04-03 02:34:06 +0300

    comments about solution

--- tutorial/optionalChallenges/slidingWindow/longestSubstring/myfindlongestSubString.js
+++ tutorial/optionalChallenges/slidingWindow/longestSubstring/myfindlongestSubString.js
@@ -6,15 +6,16 @@ function findLongestSubstring(str) {
     for (let i = 0; i < str.length; i++) {
       let char = str[i];
       if (seen[char]) {
-        start = Math.max(start, seen[char]);
+        start = Math.max(start, seen[char]);//start reassigned multiple times
     //   console.log(start); // just confirming
      // index - beginning of substring + 1 (to include current in count)
-     longest = Math.max(longest, i - start + 1);
+     longest = Math.max(longest, i - start + 1); //reassigned multiple times
       }
      
       // store the index of the next char so as to not double count
       seen[char] = i + 1;
-      console.log(seen);
+      console.log(start);
+    //   console.log(seen);
     }
     console.log(longest);
     
