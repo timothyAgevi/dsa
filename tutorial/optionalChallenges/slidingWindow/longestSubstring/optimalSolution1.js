b824763353a2aecfb2e00a29f5e4929d5b3770bd
timothyAgevi <timothyagevi@gmail.com>
2022-04-03 22:14:11 +0300

    edge case

--- tutorial/optionalChallenges/slidingWindow/longestSubstring/optimalSolution1.js
+++ tutorial/optionalChallenges/slidingWindow/longestSubstring/optimalSolution1.js
@@ -1,7 +1,8 @@
 var lengthOfLongestSubstring = function(s) {
     let max_len = 0;
-      let curr = 0;//tracks substring 
+      let currLength = 0;//tracks substring 
       let hash = {}; //contars xters:index
+      //edge case 
       if(s.length < 2) {
           return s.length;
       }
@@ -9,16 +10,16 @@ var lengthOfLongestSubstring = function(s) {
           var key = s[i];//key is the character
           
           //check if key not in the hash, add 1 to value of curr
-          if(hash[key] == null) {//hash[key] is the value(index of each xter)
-              curr += 1;//using 1st test curr is at 3
+          if( hash[key]== null) {//hash[key] is the value(index of each xter)
+              currLength += 1;//using 1st test curr is at 3
           } else {
-              curr = Math.min(i - hash[key], curr + 1);//3,4 current curl is 4 since not yet updated
+              currLength = Math.min(i - hash[key], currLength + 1);//3,4 current curl is 4 since not yet updated
           }
-          max_len = Math.max(max_len, curr);
+          max_len = Math.max(max_len, currLength);
           //if key already in map update index after ,performng the codes above
            //save the index
            hash[key] = i;
-          console.log(hash)
+          console.log(hash);
       }
       
       console.log(max_len);
