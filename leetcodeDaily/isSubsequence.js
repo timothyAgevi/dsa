ad9704557a537f435b7d383f8859b5f33397b687
timothyAgevi <timothyagevi@gmail.com>
2022-04-03 23:25:42 +0300

    solution

--- leetcodeDaily/isSubsequence.js
+++ leetcodeDaily/isSubsequence.js
@@ -19,3 +19,24 @@ Constraints:
 0 <= s.length <= 100
 0 <= t.length <= 104
 s and t consist only of lowercase English letters.*/
+
+var isSubsequence = function(s, t) {
+    //edge case
+      if (s.length == 0) {
+          return true 
+      };
+      //using two pointers
+      let p1 = 0 , p2 = 0;
+      while(p1 < s.length && p2 < t.length) { 
+          if (s.charAt(p1) == t.charAt(p2)) {//check if charetrs are similar
+              //increment to next indexes
+              ++p1;
+          }   ++p2;
+          //
+          if (p1 >= s.length){
+              return true
+          }
+      };
+      return false
+  
+  };
