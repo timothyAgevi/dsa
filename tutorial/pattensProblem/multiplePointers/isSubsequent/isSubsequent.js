40e974d0271d784f7b8e36cb6777bb1c702b08b2
timothyAgevi <timothyagevi@gmail.com>
2022-08-15 15:23:42 +0300

    isSubsequent pointer solution

--- tutorial/pattensProblem/multiplePointers/isSubsequent/isSubsequent.js
+++ tutorial/pattensProblem/multiplePointers/isSubsequent/isSubsequent.js
@@ -2,7 +2,9 @@
 
 // Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace"
+// A subsequence of a string is a new string that is formed from the original string 
+// by deleting some (can be none) 
+// of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace"
 //  is a subsequence of "abcde" while "aec" is not).
 
 var isSubsequence = function(s, t) {
@@ -10,7 +12,7 @@ var isSubsequence = function(s, t) {
     if (s.length == 0) {
         return true 
     };
-    
+    //p1 and p2 are indexes at sting s and t respectively
     let p1 = 0 , p2 = 0;
     while(p1 < s.length && p2 < t.length) { 
         if (s.charAt(p1) == t.charAt(p2)) {
@@ -24,3 +26,7 @@ var isSubsequence = function(s, t) {
     return false
 
 };
+
+
+
+//NOTE :USE THIS APPROACH INSTEAD OF FREQENCY COUNTER
