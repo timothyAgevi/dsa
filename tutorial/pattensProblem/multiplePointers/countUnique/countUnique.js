77eeece472a8cfd99cb3bf498267962f89fa24f4
timothyAgevi <timothyagevi@gmail.com>
2022-09-14 00:29:14 +0300

    countUnique values using while loop

--- tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
+++ tutorial/pattensProblem/multiplePointers/countUnique/countUnique.js
@@ -2,16 +2,18 @@
 //and counts the unique values in the array.
 //there can be negative numbers in array,but it will be always sorted.
 function countUniqueValues(arr){
+
     if(arr.length === 0) return 0;
+
     var i = 0;// index at zero  
     for(var j = 1; j < arr.length; j++){
         if(arr[i] !== arr[j]){
             i++;
-            arr[i] = arr[j];                   
+            // arr[i] = arr[j];                   
                 }
     }
 
-    // console.log(i + 1)
+    console.log(i + 1)
     return i + 1;
     
 }
@@ -20,3 +22,24 @@ countUniqueValues([1,2,3])
 //countUniqueValues([1,2,3,4,4,5]) 1,2,3,5
 
 // try without changing array.
+
+//using while loop
+function countUniqueValues(arr){
+
+    if(arr.length === 0) return 0;
+
+    var i = 0;// index at zero  
+    var j=1;
+   while( j <arr.length){
+        if(arr[i] !== arr[j]){
+            i++;
+            j++;
+            // arr[i] = arr[j];                   
+                }
+    }
+
+    console.log(j);
+    return j;
+    
+}
+countUniqueValues([1,2,3])
